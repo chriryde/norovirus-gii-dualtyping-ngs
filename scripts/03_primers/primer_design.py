2033ec7cf833fbfebf6240050e9f77f73afd20f9
simonholmgren02 <simon.holmgren.02@gmail.com>
2026-04-24 16:12:04 +0200

    Implemented filter for primer candidates.

--- scripts/03_primers/primer_design.py
+++ scripts/03_primers/primer_design.py
@@ -1,6 +1,11 @@
 import yaml
 import subprocess
+import json
+import csv
+
+from collections import defaultdict
 from pathlib import Path
+
 
 PRIMER_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT = (PRIMER_DIR.parent).parent
@@ -16,7 +21,8 @@ INPUT_FASTA = PROJECT_ROOT / paths_config["input_fasta"]
 #OUTPUT_EXCLUDED = PROJECT_ROOT / paths_config["output_excluded"]
 OUTPUT_DIR = PROJECT_ROOT / paths_config["output_dir"]
 OUTPUT_DIR_NAME = PROJECT_ROOT / paths_config["output_dir"] / "test_output"
-TVS_PRIMERS = PROJECT_ROOT / paths_config["output_dir"] / "test_output" / 'primers.tvs'
+TSV_PRIMERS = PROJECT_ROOT / paths_config["output_dir"] / "test_output" / "primers.tsv"
+TSV_FILTERED_PRIMERS = PROJECT_ROOT / paths_config["output_dir"] / "test_output" / "filtered_primers.tsv"
 
 config_varvamp = config["varvamp"]
 scheme = config_varvamp["scheme"]
@@ -39,13 +45,37 @@ except subprocess.CalledProcessError as err:
     print("stdout", err.stdout)
     print("stderr", err.stderr)
     raise
-print('avslutar primerdesign')
+print('avslutar primerdesign') 
 
 
 print('removing primers outside bp 4000-6500')
 
-with open(TVS_PRIMERS, 'r') as file:
-    for line in file:
+all_primers = set()
+excluded_dict = set()
+counter = 0
+
+with open(TSV_PRIMERS, 'r') as file:
+    tsv_reader = csv.reader(file, delimiter='\t')
+    for row in tsv_reader:
+        if counter != 0:
+            all_primers.add(tuple(row))
+            start = int(row[5])
+            if type(start) is int:
+                if start < 4500 or start > 6000:
+                    excluded_dict.add(tuple(row))
+        counter += 1
+    
+    curated_primers = all_primers - excluded_dict
+    
+    header = ('amlicon_name', 'amplicon_length', 'primer_name', 'primer_name_all_primers', 
+               'pool', 'start', 'stop', 'seq', 'size', 'gc_best', 'temp_best', 'mean_gc', 
+                 'mean_temp', 'penalty', 'off_target_amplicons')
+    
+    with open(TSV_FILTERED_PRIMERS, 'w', newline='') as outfile:
+        writer = csv.writer(outfile, delimiter='\t')
+        writer.writerow(header)
+        writer.writerows(sorted(curated_primers))
+    print(f'Total amount of potential primers after filtration: {len(curated_primers)}')    
 
 print('procedure proceded without errors')
 # def load_config(path="fetch_to_primer.yml"):
