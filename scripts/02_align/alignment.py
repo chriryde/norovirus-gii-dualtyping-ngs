a971d55c1fee2a655f86f87be302827e76c86cb7
idlar04 <ida.larssonn@gmail.com>
2026-04-24 14:04:06 +0200

    finished draft for finding rdrp and vp1 regions and saving of mafft log file

--- scripts/02_align/alignment.py
+++ scripts/02_align/alignment.py
@@ -23,17 +23,22 @@ INPUT_FASTA = PROJECT_ROOT / paths_config["input_fasta"]
 #QC_DIR = PROJECT_ROOT / paths_config["qc_dir"]
 #OUTPUT_EXCLUDED = PROJECT_ROOT / paths_config["output_excluded"]
 OUTPUT_FASTA = PROJECT_ROOT / paths_config["output_fasta"]
+GLOBAL_LOG = PROJECT_ROOT / paths_config['global_log']
 
 try:
     print("running mafft")
-    with open(OUTPUT_FASTA, "w") as out_f:
+    with open(OUTPUT_FASTA, "w") as out_f, open(GLOBAL_LOG, "w") as out_log:
         mafft_cmd = ["mafft", "--auto", str(INPUT_FASTA)]
-        length_filtered = subprocess.run(
+        alignment = subprocess.run(
             mafft_cmd,
             stdout=out_f,
-            stderr=subprocess.PIPE,
+            # stderr=subprocess.PIPE,
+            stderr=out_log,
             check=True
         )
+        # print(alignment.stderr.decode())
+        # out_log = alignment.stderr.decode()
+
 except subprocess.CalledProcessError as err:
     print("Command failed:")
     print("cmd", err.cmd)
@@ -42,3 +47,13 @@ except subprocess.CalledProcessError as err:
     print("stderr", err.stderr)
     raise
 print(f"Process finished without errors\n")
+
+
+with open(GLOBAL_LOG, "r") as in_handle:
+    lines = in_handle.readlines()
+    for index, line in enumerate(lines):
+        if lines[index] == "Strategy:\n":
+            print(lines[index])
+            print(lines[index+1])
+            print(lines[index+2])
+
--- scripts/02_align/tools_2.py
+++ scripts/02_align/tools_2.py
@@ -4,6 +4,7 @@ import yaml
 import pandas as pd
 from Bio import SeqIO
 from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
 
 
@@ -21,40 +22,50 @@ paths_config = config["paths"]
 INPUT_METADATA = PROJECT_ROOT / paths_config["input_metadata"]
 INPUT_FASTA = PROJECT_ROOT / paths_config['input_fasta']
 RDRP_FASTA = PROJECT_ROOT / paths_config['rdrp_fasta']
+VP1_FASTA = PROJECT_ROOT / paths_config['vp1_fasta']
 
 df = pd.read_csv(INPUT_METADATA)
 
 rdrp_dict = {}
+vp1_dict = {}
 
+# check that all names match the csv file
 for index, row in df.iterrows():
     rdrp_dict[row['accession']] = (row['rdrp_start'], row['rdrp_end'])
-
-# selected_col = df.loc[:, ['accession', 'rdrp_start', 'rdrp_end']]
-# print(selected_col)
-# print(rdrp_dict)
+    vp1_dict[row['accession']] = (row['vp1_start'], row['vp1_end'])
 
 
 
-# with open(INPUT_FASTA) as in_handle, open(OUTPUT_FASTA, "w") as out_handle:
-#     for record in SeqIO.parse(in_handle, "fasta"):
-#         accession = record.id
-
-#         if accession in kept_accessions:
-#             SeqIO.write(record, out_handle, "fasta")
-
-with open(str(INPUT_FASTA)) as in_handle, open(RDRP_FASTA, "w") as out_handle:
+with open(str(INPUT_FASTA)) as in_handle, open(RDRP_FASTA, "w") as out_handle1, open(VP1_FASTA, "w") as out_handle2:
     for record in SeqIO.parse(in_handle, "fasta"):
         accession = record.id
         seq = str(record.seq).upper()
 
-        start = rdrp_dict[accession][0]
-        end = rdrp_dict[accession][1]
-        region = seq[start: end+1]
-        # print(record)
-        # print(region)
-        print()
+        rdrp_start = rdrp_dict[accession][0]
+        rdrp_end = rdrp_dict[accession][1]
+        rdrp_region = seq[rdrp_start: rdrp_end+1]
+       
+        rdrp_record = SeqRecord(
+            Seq(rdrp_region),
+            id = accession,
+            name = accession,
+            description = record.description
+        )
 
-        SeqIO.write(Seq(region), out_handle, "fasta")
+        vp1_start = vp1_dict[accession][0]
+        vp1_end = vp1_dict[accession][1]
+        vp1_region = seq[vp1_start : vp1_end+1]
+
+        vp1_record = SeqRecord(
+            Seq(vp1_region),
+            id = accession,
+            name = accession,
+            description = record.description
+        )
+       
+        SeqIO.write(rdrp_record, out_handle1, "fasta")
+        SeqIO.write(vp1_record, out_handle2, "fasta")
+
 
 
 
