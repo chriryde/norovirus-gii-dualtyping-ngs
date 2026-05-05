a3060731457dbb84acdc252836750b090414d989
simonholmgren02 <simon.holmgren.02@gmail.com>
2026-05-05 16:26:20 +0200

    Optimized primer_design
    Co-authored-by: karoili <karoili@users.noreply.github.com>

--- scripts/03_primers/primer_design.py
+++ scripts/03_primers/primer_design.py
@@ -1,6 +1,7 @@
 import yaml
 import subprocess
 import csv
+import primer_tools as pt
 
 from collections import defaultdict
 from pathlib import Path
@@ -47,166 +48,30 @@ max_length_rdrp = config_varvamp["max_length_rdrp"]
 #n_ambig = config_varvamp["n_ambig"]
 
 #Run varVamp through linux with reference library
-print('startar primerdesign complete genome')
-try:
-    cmd = ['varvamp', str(scheme), '-ol', str(opt_length), '-ml', str(max_length),
-            '-db', str(REFERENCE_LIBRARY), str(INPUT_FASTA), str(OUTPUT_DIR_NAME_COMPLETE)]
-    deduplicated_filtered = subprocess.run(
-        cmd,
-        check=True
-    )
-except subprocess.CalledProcessError as err:
-    print("Command failed:")
-    print("cmd", err.cmd)
-    print("returncode", err.returncode)
-    print("stdout", err.stdout)
-    print("stderr", err.stderr)
-    raise
-print('avslutar primerdesign') 
 
+OUTPUT_COMPLETE_DIR = pt.varvamp(scheme, opt_length, max_length,
+            REFERENCE_LIBRARY, INPUT_FASTA, OUTPUT_DIR, 'complete')
 
-print('removing primers outside bp 4500-6000')
-
-all_primers = set()
-excluded_dict = set()
-counter = 0
-
-#open and filter the output file from varVamp
-with open(TSV_PRIMERS, 'r') as file:
-    tsv_reader = csv.reader(file, delimiter='\t')
-    for row in tsv_reader:
-        if counter != 0:
-            all_primers.add(tuple(row))
-            start = int(row[5])
-            if type(start) is int:
-                if start < 4500 or start > 6000:
-                    excluded_dict.add(tuple(row))
-        counter += 1
-    
-    curated_primers = all_primers - excluded_dict
-
-#create new tsv file with filtered primers   
-    header = ('amplicon_name', 'amplicon_length', 'primer_name', 'primer_name_all_primers', 
-               'pool', 'start', 'stop', 'seq', 'size', 'gc_best', 'temp_best', 'mean_gc', 
-                 'mean_temp', 'penalty', 'off_target_amplicons')
-    
-    with open(TSV_FILTERED_PRIMERS, 'w', newline='') as outfile:
-        writer = csv.writer(outfile, delimiter='\t')
-        writer.writerow(header)
-        writer.writerows(sorted(curated_primers))
-    print(f'Total amount of potential primers after filtration: {len(curated_primers)}')    
-
-print('procedure proceded without errors')
-print('begin primer design for RDRP')
+TSV_PRIMERS_COMPLETE = OUTPUT_COMPLETE_DIR / "primers.tsv"
+pt.filter(TSV_PRIMERS_COMPLETE, OUTPUT_COMPLETE_DIR)
 
 
 #varVamp for RDRP
 
 
-#Run varVamp through linux with reference library
-print('startar primerdesign för rdrp')
-try:
-    cmd = ['varvamp', str(scheme), '-ol', str(opt_length_rdrp), '-ml', str(max_length_rdrp),
-            '-db', str(REFERENCE_LIBRARY), str(INPUT_FASTA_RDRP), str(OUTPUT_DIR_NAME_RDRP)]
-    deduplicated_filtered = subprocess.run(
-        cmd,
-        check=True
-    )
-except subprocess.CalledProcessError as err:
-    print("Command failed:")
-    print("cmd", err.cmd)
-    print("returncode", err.returncode)
-    print("stdout", err.stdout)
-    print("stderr", err.stderr)
-    raise
-print('avslutar primerdesign') 
+OUTPUT_RDRP_DIR = pt.varvamp(scheme, opt_length_rdrp, max_length_rdrp, REFERENCE_LIBRARY,
+            INPUT_FASTA_RDRP, OUTPUT_DIR, 'rdrp')
 
-
-print('removing primers outside bp 4500-6000')
-
-all_primers = set()
-excluded_dict = set()
-counter = 0
-
-#open and filter the output file from varVamp
-with open(TSV_PRIMERS_RDRP, 'r') as file:
-    tsv_reader = csv.reader(file, delimiter='\t')
-    for row in tsv_reader:
-        if counter != 0:
-            all_primers.add(tuple(row))
-            start = int(row[5])
-            if type(start) is int:
-                if start < 4500 or start > 6000:
-                    excluded_dict.add(tuple(row))
-        counter += 1
-    
-    curated_primers = all_primers - excluded_dict
-
-#create new tsc file with filtered primers   
-    header = ('amplicon_name', 'amplicon_length', 'primer_name', 'primer_name_all_primers', 
-               'pool', 'start', 'stop', 'seq', 'size', 'gc_best', 'temp_best', 'mean_gc', 
-                 'mean_temp', 'penalty', 'off_target_amplicons')
-    
-    with open(TSV_FILTERED_PRIMERS_RDRP, 'w', newline='') as outfile:
-        writer = csv.writer(outfile, delimiter='\t')
-        writer.writerow(header)
-        writer.writerows(sorted(curated_primers))
-    print(f'Total amount of potential primers after filtration: {len(curated_primers)}')    
-
-print('procedure proceded without errors')
+TSV_PRIMERS_RDRP = OUTPUT_RDRP_DIR / "primers.tsv"
+pt.filter(TSV_PRIMERS_RDRP, OUTPUT_RDRP_DIR)
 
 
 #varVamp for VP1
 
-
 #Run varVamp through linux with reference library
-print('startar primerdesign för vp1')
-try:
-    cmd = ['varvamp', str(scheme), '-ol', str(opt_length_rdrp), '-ml', str(max_length_rdrp),
-            '-db', str(REFERENCE_LIBRARY), str(INPUT_FASTA_VP1), str(OUTPUT_DIR_NAME_VP1)]
-    deduplicated_filtered = subprocess.run(
-        cmd,
-        check=True
-    )
-except subprocess.CalledProcessError as err:
-    print("Command failed:")
-    print("cmd", err.cmd)
-    print("returncode", err.returncode)
-    print("stdout", err.stdout)
-    print("stderr", err.stderr)
-    raise
-print('avslutar primerdesign') 
+OUTPUT_VP1_DIR = pt.varvamp(scheme, opt_length_rdrp, max_length_rdrp,
+            REFERENCE_LIBRARY, INPUT_FASTA_VP1, OUTPUT_DIR, 'vp1')
 
+TSV_PRIMERS_VP1 = OUTPUT_VP1_DIR / "primers.tsv"
+pt.filter(TSV_PRIMERS_VP1, OUTPUT_VP1_DIR)
 
-print('removing primers outside bp 4000-6500')
-
-all_primers = set()
-excluded_dict = set()
-counter = 0
-
-#open and filter the output file from varVamp
-with open(TSV_PRIMERS_VP1, 'r') as file:
-    tsv_reader = csv.reader(file, delimiter='\t')
-    for row in tsv_reader:
-        if counter != 0:
-            all_primers.add(tuple(row))
-            start = int(row[5])
-            if type(start) is int:
-                if start < 4500 or start > 6000:
-                    excluded_dict.add(tuple(row))
-        counter += 1
-    
-    curated_primers = all_primers - excluded_dict
-
-#create new tsc file with filtered primers   
-    header = ('amplicon_name', 'amplicon_length', 'primer_name', 'primer_name_all_primers', 
-               'pool', 'start', 'stop', 'seq', 'size', 'gc_best', 'temp_best', 'mean_gc', 
-                 'mean_temp', 'penalty', 'off_target_amplicons')
-    
-    with open(TSV_FILTERED_PRIMERS_VP1, 'w', newline='') as outfile:
-        writer = csv.writer(outfile, delimiter='\t')
-        writer.writerow(header)
-        writer.writerows(sorted(curated_primers))
-    print(f'Total amount of potential primers after filtration: {len(curated_primers)}')    
-
-print('procedure proceded without errors')
--- scripts/03_primers/quality_control.py
+++ scripts/03_primers/quality_control.py
@@ -10,40 +10,78 @@ with open(CONFIG_PATH, "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
 TEMPORARY_WEB_CRAWLER_DATA_CSV = PROJECT_ROOT / Path(config["paths"]["web_crawler_data"]) / "test_typing_data.csv"
-REFERENCE_SEQUENCE_CSV = Path(config["paths"]["metadata_dir"]) 
+REFERENCE_SEQUENCE_CSV = Path(config["paths"]["metadata_dir"])
 
 all_typed_sequences = {}
 all_reference_sequences = {}
 matches = []
 missmatches = []
+matches_rdrp = []
+missmatches_rdrp = []
+matches_vp1= []
+missmatches_vp1 = []
 
 
 
 with open(TEMPORARY_WEB_CRAWLER_DATA_CSV, 'r') as file:
-    csv_reader = csv.reader(file, delimiter=',')
+    csv_reader = csv.DictReader(file, delimiter=',')
     next(csv_reader)
     for row in csv_reader:
-        ascension_nr = str(row[0])
-        polymerase_type = str(row[8])
-        polymerase_subtype = str(row[14])
+        ascension_nr = str(row['name']).split("_region")[0]
+        polymerase_type = str(row['polymerase type'])
+        polymerase_subtype = str(row['capsid type'])
         #lägg till alla typningar i set
         all_typed_sequences[ascension_nr] = [polymerase_type, polymerase_subtype]
 
 
 with open(REFERENCE_SEQUENCE_CSV, 'r') as file:
-    csv_reader = csv.reader(file, delimiter=',')
+    csv_reader = csv.DictReader(file, delimiter=',')
     next(csv_reader)
     for row in csv_reader:
-        ascension_nr = str(row[0])
-        polymerase_type = str(row[8])
-        polymerase_subtype = str(row[14])
+        ascension_nr = str(row['accession'])
+        polymerase_type = str(row['p_type'])
+        polymerase_subtype = str(row['genotype'])
         #lägg till alla typningar i set
-        all_typed_sequences[ascension_nr] = [polymerase_type, polymerase_subtype]
+        all_reference_sequences[ascension_nr] = [polymerase_type, polymerase_subtype]
 
-for typed_sequence, reference_sequence in zip(all_typed_sequences, all_reference_sequences):
-    if typed_sequence == reference_sequence:
-        matches.append(typed_sequence)
+
+#check matches for both
+for typed_sequence in all_typed_sequences.keys():
+    if typed_sequence in all_reference_sequences:
+        if all_typed_sequences[typed_sequence] == 'Could not assign':
+            continue
+        if all_typed_sequences[typed_sequence] == all_reference_sequences[typed_sequence]:
+            matches.append(typed_sequence)
+        else:
+            missmatches.append(typed_sequence)
     else:
-        missmatches.append(typed_sequence)
+        print(f'error: accession number {typed_sequence} does not exist in reference file')
+        continue
+
+#check matches for rdrp
+for typed_sequence in all_typed_sequences.keys():
+    if typed_sequence in all_reference_sequences:
+        if all_typed_sequences[typed_sequence] == 'Could not assign':
+            continue
+        if all_typed_sequences[typed_sequence][0] == all_reference_sequences[typed_sequence][0]:
+            matches_rdrp.append(typed_sequence)
+        else:
+            missmatches_rdrp.append(typed_sequence)
+    else:
+        continue
+
+#check for matches in vp1
+for typed_sequence in all_typed_sequences.keys():
+    if typed_sequence in all_reference_sequences:
+        if all_typed_sequences[typed_sequence] == 'Could not assign':
+            continue
+        if all_typed_sequences[typed_sequence][1] == all_reference_sequences[typed_sequence][1]:
+            matches_vp1.append(typed_sequence)
+        else:
+            missmatches_vp1.append(typed_sequence)
+    else:
+        continue
 
 print(f'Ratio of sequences correctly typed: {len(matches) / len(all_typed_sequences)}')
+print(f'Ratio of rdrp:s correctly typed: {len(matches_rdrp) / len(all_typed_sequences)}')
+print(f'Ratio of vp1:s correctly typed: {len(matches_vp1) / len(all_typed_sequences)}')
